9a964bcd8355bfa6fc1adecb94488e9183fa0328
607163943 <q45962219@126.com>
2026-06-11 12:49:12 +0800

    refactor(db):修改SQL脚本以适应MySQL8

--- static-files/my_blog_db.sql
+++ static-files/my_blog_db.sql
@@ -1,11 +1,11 @@
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_blog_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`my_blog_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
 USE `my_blog_db`;
 
@@ -14,10 +14,10 @@ USE `my_blog_db`;
 DROP TABLE IF EXISTS `generator_test`;
 
 CREATE TABLE `generator_test` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
   `test` varchar(100) NOT NULL COMMENT '测试字段',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `generator_test` */
 
@@ -28,7 +28,7 @@ DROP TABLE IF EXISTS `jdbc_test`;
 CREATE TABLE `jdbc_test` (
   `type` varchar(100) DEFAULT NULL COMMENT '类型',
   `name` varchar(100) DEFAULT NULL COMMENT '名称'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `jdbc_test` */
 
@@ -42,13 +42,13 @@ insert  into `jdbc_test`(`type`,`name`) values ('类别2','测试类2');
 DROP TABLE IF EXISTS `tb_admin_user`;
 
 CREATE TABLE `tb_admin_user` (
-  `admin_user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `admin_user_id` int NOT NULL AUTO_INCREMENT COMMENT '管理员id',
   `login_user_name` varchar(50) NOT NULL COMMENT '管理员登陆名称',
   `login_password` varchar(50) NOT NULL COMMENT '管理员登陆密码',
   `nick_name` varchar(50) NOT NULL COMMENT '管理员显示昵称',
-  `locked` tinyint(4) DEFAULT '0' COMMENT '是否锁定 0未锁定 1已锁定无法登陆',
+  `locked` tinyint DEFAULT '0' COMMENT '是否锁定 0未锁定 1已锁定无法登陆',
   PRIMARY KEY (`admin_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_admin_user` */
 
@@ -59,22 +59,22 @@ insert  into `tb_admin_user`(`admin_user_id`,`login_user_name`,`login_password`,
 DROP TABLE IF EXISTS `tb_blog`;
 
 CREATE TABLE `tb_blog` (
-  `blog_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '博客表主键id',
+  `blog_id` bigint NOT NULL AUTO_INCREMENT COMMENT '博客表主键id',
   `blog_title` varchar(200) NOT NULL COMMENT '博客标题',
   `blog_sub_url` varchar(200) NOT NULL COMMENT '博客自定义路径url',
   `blog_cover_image` varchar(200) NOT NULL COMMENT '博客封面图',
   `blog_content` mediumtext NOT NULL COMMENT '博客内容',
-  `blog_category_id` int(11) NOT NULL COMMENT '博客分类id',
+  `blog_category_id` int NOT NULL COMMENT '博客分类id',
   `blog_category_name` varchar(50) NOT NULL COMMENT '博客分类(冗余字段)',
   `blog_tags` varchar(200) NOT NULL COMMENT '博客标签',
-  `blog_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-草稿 1-发布',
-  `blog_views` bigint(20) NOT NULL DEFAULT '0' COMMENT '阅读量',
-  `enable_comment` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0-允许评论 1-不允许评论',
-  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `blog_status` tinyint NOT NULL DEFAULT '0' COMMENT '0-草稿 1-发布',
+  `blog_views` bigint NOT NULL DEFAULT '0' COMMENT '阅读量',
+  `enable_comment` tinyint NOT NULL DEFAULT '0' COMMENT '0-允许评论 1-不允许评论',
+  `is_deleted` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`blog_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_blog` */
 
@@ -88,14 +88,14 @@ insert  into `tb_blog`(`blog_id`,`blog_title`,`blog_sub_url`,`blog_cover_image`,
 DROP TABLE IF EXISTS `tb_blog_category`;
 
 CREATE TABLE `tb_blog_category` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '分类表主键',
+  `category_id` int NOT NULL AUTO_INCREMENT COMMENT '分类表主键',
   `category_name` varchar(50) NOT NULL COMMENT '分类的名称',
   `category_icon` varchar(50) NOT NULL COMMENT '分类的图标',
-  `category_rank` int(11) NOT NULL DEFAULT '1' COMMENT '分类的排序值 被使用的越多数值越大',
-  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `category_rank` int NOT NULL DEFAULT '1' COMMENT '分类的排序值 被使用的越多数值越大',
+  `is_deleted` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_blog_category` */
 
@@ -108,8 +108,8 @@ insert  into `tb_blog_category`(`category_id`,`category_name`,`category_icon`,`c
 DROP TABLE IF EXISTS `tb_blog_comment`;
 
 CREATE TABLE `tb_blog_comment` (
-  `comment_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `blog_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '关联的blog主键',
+  `comment_id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `blog_id` bigint NOT NULL DEFAULT '0' COMMENT '关联的blog主键',
   `commentator` varchar(50) NOT NULL DEFAULT '' COMMENT '评论者名称',
   `email` varchar(100) NOT NULL DEFAULT '' COMMENT '评论人的邮箱',
   `website_url` varchar(50) NOT NULL DEFAULT '' COMMENT '网址',
@@ -118,10 +118,10 @@ CREATE TABLE `tb_blog_comment` (
   `commentator_ip` varchar(20) NOT NULL DEFAULT '' COMMENT '评论时的ip地址',
   `reply_body` varchar(200) NOT NULL DEFAULT '' COMMENT '回复内容',
   `reply_create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '回复时间',
-  `comment_status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否审核通过 0-未审核 1-审核通过',
-  `is_deleted` tinyint(4) DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
+  `comment_status` tinyint NOT NULL DEFAULT '0' COMMENT '是否审核通过 0-未审核 1-审核通过',
+  `is_deleted` tinyint DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
   PRIMARY KEY (`comment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_blog_comment` */
 
@@ -132,12 +132,12 @@ insert  into `tb_blog_comment`(`comment_id`,`blog_id`,`commentator`,`email`,`web
 DROP TABLE IF EXISTS `tb_blog_tag`;
 
 CREATE TABLE `tb_blog_tag` (
-  `tag_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签表主键id',
+  `tag_id` int NOT NULL AUTO_INCREMENT COMMENT '标签表主键id',
   `tag_name` varchar(100) NOT NULL COMMENT '标签名称',
-  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
+  `is_deleted` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除 0=否 1=是',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`tag_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_blog_tag` */
 
@@ -159,12 +159,12 @@ insert  into `tb_blog_tag`(`tag_id`,`tag_name`,`is_deleted`,`create_time`) value
 DROP TABLE IF EXISTS `tb_blog_tag_relation`;
 
 CREATE TABLE `tb_blog_tag_relation` (
-  `relation_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '关系表id',
-  `blog_id` bigint(20) NOT NULL COMMENT '博客id',
-  `tag_id` int(11) NOT NULL COMMENT '标签id',
+  `relation_id` bigint NOT NULL AUTO_INCREMENT COMMENT '关系表id',
+  `blog_id` bigint NOT NULL COMMENT '博客id',
+  `tag_id` int NOT NULL COMMENT '标签id',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
   PRIMARY KEY (`relation_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_blog_tag_relation` */
 
@@ -191,7 +191,7 @@ CREATE TABLE `tb_config` (
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
   PRIMARY KEY (`config_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_config` */
 
@@ -213,16 +213,16 @@ insert  into `tb_config`(`config_name`,`config_value`,`create_time`,`update_time
 DROP TABLE IF EXISTS `tb_link`;
 
 CREATE TABLE `tb_link` (
-  `link_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '友链表主键id',
-  `link_type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '友链类别 0-友链 1-推荐 2-个人网站',
+  `link_id` int NOT NULL AUTO_INCREMENT COMMENT '友链表主键id',
+  `link_type` tinyint NOT NULL DEFAULT '0' COMMENT '友链类别 0-友链 1-推荐 2-个人网站',
   `link_name` varchar(50) NOT NULL COMMENT '网站名称',
   `link_url` varchar(100) NOT NULL COMMENT '网站链接',
   `link_description` varchar(100) NOT NULL COMMENT '网站描述',
-  `link_rank` int(11) NOT NULL DEFAULT '0' COMMENT '用于列表排序',
-  `is_deleted` tinyint(4) NOT NULL DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
+  `link_rank` int NOT NULL DEFAULT '0' COMMENT '用于列表排序',
+  `is_deleted` tinyint NOT NULL DEFAULT '0' COMMENT '是否删除 0-未删除 1-已删除',
   `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
   PRIMARY KEY (`link_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_link` */
 
@@ -251,10 +251,10 @@ insert  into `tb_link`(`link_id`,`link_type`,`link_name`,`link_url`,`link_descri
 DROP TABLE IF EXISTS `tb_test`;
 
 CREATE TABLE `tb_test` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
   `test_info` varchar(50) NOT NULL COMMENT '测试内容',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `tb_test` */
 
